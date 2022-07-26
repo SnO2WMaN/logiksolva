5aef75e58630bdd8a53569105215ef5daf1082ae
SnO2WMaN <me@sno2wman.net>
2022-07-26 20:24:42 +0900

    notnot (!!P=P)

--- mod.ts
+++ mod.ts
@@ -1,3 +1,41 @@
-import { green, italic } from "std/fmt/colors.ts";
+import { assertEquals } from "std/testing/asserts.ts";
 
-console.log(`Hello ${italic(green("World"))}!`);
+type FormulaPart =
+  | { type: "PROP"; id: string }
+  | { type: "NOT"; include: FormulaPart }
+  | { type: "AND"; left: FormulaPart; right: FormulaPart }
+  | { type: "OR"; left: FormulaPart; right: FormulaPart }
+  | { type: "IMPLICT"; left: FormulaPart; right: FormulaPart };
+
+const formula: FormulaPart = {
+  type: "NOT",
+  include: {
+    type: "PROP",
+    id: "P",
+  },
+};
+
+const notnot = (f: FormulaPart) => {
+  if (f.type === "NOT" && f.include.type === "NOT") return f.include.include;
+  return f;
+};
+
+Deno.test("propositional:notnot:1", () => {
+  // !!P = P
+  const actual = notnot({ type: "NOT", include: { type: "NOT", include: { type: "PROP", id: "P" } } });
+  const expected: FormulaPart = { type: "PROP", id: "P" };
+  assertEquals(
+    actual,
+    expected,
+  );
+});
+
+Deno.test("propositional:notnot:2", () => {
+  // !P nothing change
+  const actual = notnot({ type: "NOT", include: { type: "PROP", id: "P" } });
+  const expected: FormulaPart = { type: "NOT", include: { type: "PROP", id: "P" } };
+  assertEquals(
+    actual,
+    expected,
+  );
+});
