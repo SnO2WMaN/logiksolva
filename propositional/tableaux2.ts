8a469df265b9394a4c9271b9796804c27a01684a
SnO2WMaN <me@sno2wman.net>
2022-07-29 18:43:59 +0900

    tobranch and valid check

--- propositional/tableaux2.ts
+++ propositional/tableaux2.ts
@@ -1,12 +1,14 @@
 import { deepMerge } from "std/collections/mod.ts";
 import { Or, PropFormula } from "./types.ts";
 
+export type PropsTable = Record<string, { 0?: true; 1?: true }>;
 export type Branch =
   | {
     nodes: PropFormula[];
     stack: PropFormula[];
     skip: (Or)[];
     props: PropsTable;
+    valid?: boolean;
     left: null;
     right: null;
   }
@@ -15,6 +17,7 @@ export type Branch =
     stack: [];
     skip: [];
     props: PropsTable;
+    valid?: boolean;
     left: Branch;
     right: Branch;
   };
@@ -28,6 +31,7 @@ export const toBranch = (b: Branch): Branch => {
         stack: rest,
         skip: [...b.skip, head],
         props: b.props,
+        valid: b.valid,
         left: null,
         right: null,
       });
@@ -38,6 +42,7 @@ export const toBranch = (b: Branch): Branch => {
         stack: [...rest, ...next],
         skip: b.skip,
         props: deepMerge(b.props, props),
+        valid: b.valid,
         left: null,
         right: null,
       });
@@ -49,11 +54,31 @@ export const toBranch = (b: Branch): Branch => {
       stack: [],
       skip: [],
       props: b.props,
-      left: toBranch({ nodes: [], stack: [head.left], skip: rest, props: b.props, left: null, right: null }),
-      right: toBranch({ nodes: [], stack: [head.right], skip: rest, props: b.props, left: null, right: null }),
+      valid: b.valid,
+      left: toBranch({
+        nodes: [],
+        stack: [head.left],
+        skip: rest,
+        props: b.props,
+        valid: b.valid,
+        left: null,
+        right: null,
+      }),
+      right: toBranch({
+        nodes: [],
+        stack: [head.right],
+        skip: rest,
+        props: b.props,
+        valid: b.valid,
+        left: null,
+        right: null,
+      }),
     };
   } else {
-    return b;
+    return {
+      ...b,
+      valid: b.valid || Object.values(b.props).every((v) => !v["0"] || !v["1"]),
+    };
   }
 };
 
@@ -95,146 +120,17 @@ export const disassemblyFormula = (f: Exclude<PropFormula, Or>): { next: PropFor
   }
 };
 
-export type TreeNode<F extends PropFormula = PropFormula> = { eval: boolean; formula: F };
-export type PropsTable = Record<string, { 0?: true; 1?: true }>;
-
-export const step = (t: TreeNode): {
-  next: TreeNode[];
-  skip: TreeNode<Or>[];
-  props: PropsTable;
-} => {
-  if (t.formula.type === "PROP") {
-    return {
-      next: [{ ...t, eval: true }],
-      skip: [],
-      props: { [t.formula.id]: { 1: true } },
-    };
-  } else if (t.formula.type === "AND") {
-    return {
-      next: [
-        { ...t, eval: true },
-        { eval: false, formula: t.formula.left },
-        { eval: false, formula: t.formula.right },
-      ],
-      skip: [],
-      props: {},
-    };
-  } else if (t.formula.type === "OR") {
-    return {
-      next: [{ ...t, eval: true }],
-      skip: [{ eval: false, formula: t.formula }],
-      props: {},
-    };
-  } else if (t.formula.type === "IMPLICT") {
-    return {
-      next: [{ ...t, eval: true }],
-      skip: [{
-        eval: false,
-        formula: { type: "OR", left: { type: "NOT", in: t.formula.left }, right: t.formula.right },
-      }],
-      props: {},
-    };
-  } else if (t.formula.type === "NOT" && t.formula.in.type === "PROP") {
-    return {
-      next: [{ ...t, eval: true }],
-      skip: [],
-      props: { [t.formula.in.id]: { 0: true } },
-    };
-  } else if (t.formula.type === "NOT" && t.formula.in.type === "OR") {
-    return {
-      next: [
-        { ...t, eval: true },
-        {
-          eval: false,
-          formula: {
-            type: "AND",
-            left: { type: "NOT", in: t.formula.in.left },
-            right: { type: "NOT", in: t.formula.in.right },
-          },
-        },
-      ],
-      skip: [],
-      props: {},
-    };
-  } else if (t.formula.type === "NOT" && t.formula.in.type === "AND") {
-    return {
-      next: [{ ...t, eval: true }],
-      skip: [{
-        eval: false,
-        formula: {
-          type: "OR",
-          left: { type: "NOT", in: t.formula.in.left },
-          right: { type: "NOT", in: t.formula.in.right },
-        },
-      }],
-      props: {},
-    };
-  } else if (t.formula.type === "NOT" && t.formula.in.type === "IMPLICT") {
-    return {
-      next: [
-        { ...t, eval: true },
-        {
-          eval: false,
-          formula: { type: "AND", left: t.formula.in.left, right: { type: "NOT", in: t.formula.in.right } },
-        },
-      ],
-      skip: [],
-      props: {},
-    };
-  } else if (t.formula.type === "NOT" && t.formula.in.type === "NOT") {
-    return {
-      next: [
-        { ...t, eval: true },
-        { eval: false, formula: t.formula.in.in },
-      ],
-      skip: [],
-      props: {},
-    };
-  }
-
-  throw new Error("Something wrong");
-};
-
-export const serialize = (t: TreeNode): {
-  tree: TreeNode[];
-  skip: TreeNode<Or>[];
-  props: PropsTable;
-} => {
-  const { next, props, skip } = step(t);
-  return next.reduce(
-    ({ tree: pTree, props: pProps, skip: pSkip }, c) => {
-      const { tree: cTree, skip: cSkip, props: cProps } = !c.eval ? serialize(c) : { tree: [c], skip: [], props: {} };
-      return ({
-        tree: [...pTree, ...cTree],
-        skip: [...pSkip, ...cSkip],
-        props: deepMerge(pProps, cProps),
-      });
-    },
-    { tree: [] as TreeNode[], skip: skip, props },
-  );
-};
-
-const paral = (f: PropFormula) => {
-  const { tree, props, skip } = serialize({ eval: false, formula: f });
-  console.dir(skip.map((s) => [
-    serialize({ eval: false, formula: s.formula.left }),
-    serialize({ eval: false, formula: s.formula.right }),
-  ]));
-};
-paral({
-  type: "AND",
-  left: { type: "PROP", id: "P" },
-  right: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-});
-
 console.dir(
   toBranch({
     nodes: [],
     stack: [
       {
-        type: "AND",
-        left: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-        right: { type: "OR", left: { type: "PROP", id: "R" }, right: { type: "PROP", id: "S" } },
+        type: "NOT",
+        in: {
+          type: "AND",
+          left: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
+          right: { type: "OR", left: { type: "PROP", id: "R" }, right: { type: "PROP", id: "S" } },
+        },
       },
     ],
     skip: [],
@@ -252,9 +148,17 @@ console.dir(
       {
         type: "NOT",
         in: {
-          type: "AND",
-          left: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-          right: { type: "OR", left: { type: "PROP", id: "R" }, right: { type: "PROP", id: "S" } },
+          type: "IMPLICT",
+          left: {
+            type: "AND",
+            left: { type: "PROP", id: "P" },
+            right: { type: "OR", left: { type: "PROP", id: "S" }, right: { type: "PROP", id: "R" } },
+          },
+          right: {
+            type: "OR",
+            left: { type: "AND", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "S" } },
+            right: { type: "AND", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "R" } },
+          },
         },
       },
     ],
