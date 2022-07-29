cba3ad32ed6e3f7a102683e496c7a587eb8f6755
SnO2WMaN <me@sno2wman.net>
2022-07-29 18:33:56 +0900

    ブランチへの分解

--- propositional/tableaux2.ts
+++ propositional/tableaux2.ts
@@ -1,8 +1,101 @@
 import { deepMerge } from "std/collections/mod.ts";
-import { assertEquals } from "std/testing/asserts.ts";
 import { Or, PropFormula } from "./types.ts";
 
-export type TreeNode<F extends PropFormula = PropFormula> = { formula: F; eval: boolean };
+export type Branch =
+  | {
+    nodes: PropFormula[];
+    stack: PropFormula[];
+    skip: (Or)[];
+    props: PropsTable;
+    left: null;
+    right: null;
+  }
+  | {
+    nodes: PropFormula[];
+    stack: [];
+    skip: [];
+    props: PropsTable;
+    left: Branch;
+    right: Branch;
+  };
+
+export const toBranch = (b: Branch): Branch => {
+  if (0 < b.stack.length) {
+    const [head, ...rest] = b.stack;
+    if (head.type === "OR") {
+      return toBranch({
+        nodes: [...b.nodes],
+        stack: rest,
+        skip: [...b.skip, head],
+        props: b.props,
+        left: null,
+        right: null,
+      });
+    } else {
+      const { next, props } = disassemblyFormula(head);
+      return toBranch({
+        nodes: [...b.nodes, head],
+        stack: [...rest, ...next],
+        skip: b.skip,
+        props: deepMerge(b.props, props),
+        left: null,
+        right: null,
+      });
+    }
+  } else if (0 < b.skip.length) {
+    const [head, ...rest] = b.skip;
+    return {
+      nodes: [...b.nodes, head],
+      stack: [],
+      skip: [],
+      props: b.props,
+      left: toBranch({ nodes: [], stack: [head.left], skip: rest, props: b.props, left: null, right: null }),
+      right: toBranch({ nodes: [], stack: [head.right], skip: rest, props: b.props, left: null, right: null }),
+    };
+  } else {
+    return b;
+  }
+};
+
+export const disassemblyFormula = (f: Exclude<PropFormula, Or>): { next: PropFormula[]; props: PropsTable } => {
+  if (f.type === "AND") {
+    return { next: [f.left, f.right], props: {} };
+  } else if (f.type === "IMPLICT") {
+    return { next: [{ type: "OR", left: { type: "NOT", in: f.left }, right: f.right }], props: {} };
+  } else if (f.type === "NOT") {
+    switch (f.in.type) {
+      case "NOT":
+        return { next: [f.in.in], props: {} };
+      case "OR":
+        return {
+          next: [{ type: "NOT", in: f.in.left }, { type: "NOT", in: f.in.right }],
+          props: {},
+        };
+      case "AND":
+        return {
+          next: [{ type: "OR", left: { type: "NOT", in: f.in.left }, right: { type: "NOT", in: f.in.right } }],
+          props: {},
+        };
+      case "IMPLICT":
+        return {
+          next: [f.in.left, { type: "NOT", in: f.in.right }],
+          props: {},
+        };
+      case "PROP":
+        return {
+          next: [],
+          props: { [f.in.id]: { 0: true } },
+        };
+    }
+  } else {
+    return {
+      next: [],
+      props: { [f.id]: { 1: true } },
+    };
+  }
+};
+
+export type TreeNode<F extends PropFormula = PropFormula> = { eval: boolean; formula: F };
 export type PropsTable = Record<string, { 0?: true; 1?: true }>;
 
 export const step = (t: TreeNode): {
@@ -102,176 +195,6 @@ export const step = (t: TreeNode): {
   throw new Error("Something wrong");
 };
 
-Deno.test("propositional:tableaux:step:prop", () => {
-  const actual = step({ eval: false, formula: { type: "PROP", id: "P" } });
-  assertEquals(actual, {
-    next: [{ eval: true, formula: { type: "PROP", id: "P" } }],
-    skip: [],
-    props: { "P": { 1: true } },
-  });
-});
-
-Deno.test("propositional:tableaux:step:or", () => {
-  const actual = step({
-    eval: false,
-    formula: {
-      type: "OR",
-      left: { type: "PROP", id: "P" },
-      right: { type: "PROP", id: "Q" },
-    },
-  });
-  assertEquals(actual, {
-    next: [{ eval: true, formula: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } } }],
-    skip: [
-      {
-        eval: false,
-        formula: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-      },
-    ],
-    props: {},
-  });
-});
-
-Deno.test("propositional:tableaux:step:and", () => {
-  const actual = step({
-    eval: false,
-    formula: {
-      type: "AND",
-      left: { type: "PROP", id: "P" },
-      right: { type: "PROP", id: "Q" },
-    },
-  });
-  assertEquals(actual, {
-    next: [
-      { eval: true, formula: { type: "AND", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } } },
-      { eval: false, formula: { type: "PROP", id: "P" } },
-      { eval: false, formula: { type: "PROP", id: "Q" } },
-    ],
-    skip: [],
-    props: {},
-  });
-});
-
-Deno.test("propositional:tableaux:step:not_prop", () => {
-  const actual = step({
-    eval: false,
-    formula: { type: "NOT", in: { type: "PROP", id: "P" } },
-  });
-  assertEquals(actual, {
-    next: [{ eval: true, formula: { type: "NOT", in: { type: "PROP", id: "P" } } }],
-    skip: [],
-    props: { "P": { 0: true } },
-  });
-});
-
-Deno.test("propositional:tableaux:step:not_not", () => {
-  const actual = step({
-    eval: false,
-    formula: {
-      type: "NOT",
-      in: { type: "NOT", in: { type: "PROP", id: "P" } },
-    },
-  });
-  assertEquals(actual, {
-    next: [
-      { eval: true, formula: { type: "NOT", in: { type: "NOT", in: { type: "PROP", id: "P" } } } },
-      { eval: false, formula: { type: "PROP", id: "P" } },
-    ],
-    skip: [],
-    props: {},
-  });
-});
-
-Deno.test("propositional:tableaux:step:not_and", () => {
-  // !(P && Q) => !P || !Q
-  const actual = step({
-    eval: false,
-    formula: {
-      type: "NOT",
-      in: { type: "AND", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-    },
-  });
-  assertEquals(actual, {
-    next: [{
-      eval: true,
-      formula: { type: "NOT", in: { type: "AND", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } } },
-    }],
-    skip: [
-      {
-        eval: false,
-        formula: {
-          type: "OR",
-          left: { type: "NOT", in: { type: "PROP", id: "P" } },
-          right: { type: "NOT", in: { type: "PROP", id: "Q" } },
-        },
-      },
-    ],
-    props: {},
-  });
-});
-
-Deno.test("propositional:tableaux:step:not_or", () => {
-  const actual = step({
-    eval: false,
-    formula: {
-      type: "NOT",
-      in: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-    },
-  });
-  assertEquals(actual, {
-    next: [
-      {
-        eval: true,
-        formula: {
-          type: "NOT",
-          in: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-        },
-      },
-      {
-        eval: false,
-        formula: {
-          type: "AND",
-          left: { type: "NOT", in: { type: "PROP", id: "P" } },
-          right: { type: "NOT", in: { type: "PROP", id: "Q" } },
-        },
-      },
-    ],
-    skip: [],
-    props: {},
-  });
-});
-
-Deno.test("propositional:tableaux:step:not_impl", () => {
-  const actual = step({
-    eval: false,
-    formula: {
-      type: "NOT",
-      in: { type: "IMPLICT", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-    },
-  });
-  assertEquals(actual, {
-    next: [
-      {
-        eval: true,
-        formula: {
-          type: "NOT",
-          in: { type: "IMPLICT", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-        },
-      },
-      {
-        eval: false,
-        formula: {
-          type: "AND",
-          left: { type: "PROP", id: "P" },
-          right: { type: "NOT", in: { type: "PROP", id: "Q" } },
-        },
-      },
-    ],
-    skip: [],
-    props: {},
-  });
-});
-
 export const serialize = (t: TreeNode): {
   tree: TreeNode[];
   skip: TreeNode<Or>[];
@@ -291,154 +214,6 @@ export const serialize = (t: TreeNode): {
   );
 };
 
-Deno.test("propositional:tableaux:serialize:and", () => {
-  // 1-1. P && Q
-  // 1-2. P
-  // 1-3. Q
-  const actual = serialize(
-    { eval: false, formula: { type: "AND", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } } },
-  );
-  assertEquals(
-    actual,
-    {
-      tree: [
-        { eval: true, formula: { type: "AND", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } } },
-        { eval: true, formula: { type: "PROP", id: "P" } },
-        { eval: true, formula: { type: "PROP", id: "Q" } },
-      ],
-      skip: [],
-      props: {
-        P: { 1: true },
-        Q: { 1: true },
-      },
-    },
-  );
-});
-
-Deno.test("propositional:tableaux:serialize:double_and", () => {
-  // 1-1. (P && Q) && (R && S)
-  // 1-2. (P && Q)
-  // 1-2-1. P
-  // 1-2-2. Q
-  // 1-3. (R && S)
-  // 1-3-1. R
-  // 1-3-2. S
-  const actual = serialize(
-    {
-      eval: false,
-      formula: {
-        type: "AND",
-        left: { type: "AND", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-        right: { type: "AND", left: { type: "PROP", id: "R" }, right: { type: "PROP", id: "S" } },
-      },
-    },
-  );
-  assertEquals(
-    actual,
-    {
-      tree: [
-        {
-          eval: true,
-          formula: {
-            type: "AND",
-            left: { type: "AND", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-            right: { type: "AND", left: { type: "PROP", id: "R" }, right: { type: "PROP", id: "S" } },
-          },
-        },
-        { eval: true, formula: { type: "AND", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } } },
-        { eval: true, formula: { type: "PROP", id: "P" } },
-        { eval: true, formula: { type: "PROP", id: "Q" } },
-        { eval: true, formula: { type: "AND", left: { type: "PROP", id: "R" }, right: { type: "PROP", id: "S" } } },
-        { eval: true, formula: { type: "PROP", id: "R" } },
-        { eval: true, formula: { type: "PROP", id: "S" } },
-      ],
-      skip: [],
-      props: {
-        P: { 1: true },
-        Q: { 1: true },
-        R: { 1: true },
-        S: { 1: true },
-      },
-    },
-  );
-});
-
-Deno.test("propositional:tableaux:serialize:or", () => {
-  // 1-1s. P || Q
-  const actual = serialize(
-    {
-      eval: false,
-      formula: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-    },
-  );
-  assertEquals(
-    actual,
-    {
-      tree: [
-        {
-          eval: true,
-          formula: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-        },
-      ],
-      props: {},
-      skip: [
-        {
-          eval: false,
-          formula: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-        },
-      ],
-    },
-  );
-});
-
-Deno.test("propositional:tableaux:serialize:and_or", () => {
-  // 1-1. P && (P || Q)
-  // 1-2. P
-  // 1-3s. P || Q
-  const actual = serialize(
-    {
-      eval: false,
-      formula: {
-        type: "AND",
-        left: { type: "PROP", id: "P" },
-        right: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-      },
-    },
-  );
-  assertEquals(
-    actual,
-    {
-      tree: [
-        {
-          eval: true,
-          formula: {
-            type: "AND",
-            left: { type: "PROP", id: "P" },
-            right: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-          },
-        },
-        {
-          eval: true,
-          formula: { type: "PROP", id: "P" },
-        },
-        {
-          eval: true,
-          formula: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-        },
-      ],
-      skip: [
-        {
-          eval: false,
-          formula: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
-        },
-      ],
-      props: {
-        P: { 1: true },
-      },
-    },
-  );
-});
-
 const paral = (f: PropFormula) => {
   const { tree, props, skip } = serialize({ eval: false, formula: f });
   console.dir(skip.map((s) => [
@@ -451,3 +226,42 @@ paral({
   left: { type: "PROP", id: "P" },
   right: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
 });
+
+console.dir(
+  toBranch({
+    nodes: [],
+    stack: [
+      {
+        type: "AND",
+        left: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
+        right: { type: "OR", left: { type: "PROP", id: "R" }, right: { type: "PROP", id: "S" } },
+      },
+    ],
+    skip: [],
+    left: null,
+    right: null,
+    props: {},
+  }),
+  { depth: Number.MAX_SAFE_INTEGER },
+);
+
+console.dir(
+  toBranch({
+    nodes: [],
+    stack: [
+      {
+        type: "NOT",
+        in: {
+          type: "AND",
+          left: { type: "OR", left: { type: "PROP", id: "P" }, right: { type: "PROP", id: "Q" } },
+          right: { type: "OR", left: { type: "PROP", id: "R" }, right: { type: "PROP", id: "S" } },
+        },
+      },
+    ],
+    skip: [],
+    left: null,
+    right: null,
+    props: {},
+  }),
+  { depth: Number.MAX_SAFE_INTEGER },
+);
