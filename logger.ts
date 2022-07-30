6dafbd6632730041b9cb9a1b2edea0e1d115dca3
SnO2WMaN <me@sno2wman.net>
2022-07-31 01:43:44 +0900

    as server

---
+++ logger.ts
@@ -0,0 +1,15 @@
+import * as log from "std/log/mod.ts";
+
+await log.setup({
+  handlers: {
+    console: new log.handlers.ConsoleHandler("DEBUG"),
+  },
+  loggers: {
+    default: {
+      level: "DEBUG",
+      handlers: ["console"],
+    },
+  },
+});
+
+export const Logger = log.getLogger();
--- mod.ts
+++ mod.ts
@@ -1,94 +1,31 @@
-import {
-  absorptiveAndOr,
-  absorptiveOrAnd,
-  additionLeft,
-  additionRight,
-  associativeAnd,
-  associativeOr,
-  commutativeAnd,
-  commutativeOr,
-  constructiveDilemma,
-  contraposition,
-  demorganAndOr,
-  demorganOrAnd,
-  disjunctiveSyllogism,
-  distributiveAndOr,
-  distributiveOrAnd,
-  doubleNegation,
-  excludeMiddle,
-  exportation,
-  idempotentAnd,
-  idempotentOr,
-  identity,
-  importation,
-  modusPonens,
-  nonContradiction,
-  simplificationLeft,
-  simplificationRight,
-  transitive,
-} from "./propositional/tautology.ts";
-import { print } from "./propositional/mod.ts";
-import {
-  affirmingConsequent,
-  affirmingDisjunct,
-  denyingAntecedent,
-  fallancyFallancy,
-} from "./propositional/fallancy.ts";
+import { Application, Router } from "oak/mod.ts";
+import { Logger } from "./logger.ts";
+import { createFirstBranch, toBranch } from "./propositional/branch.ts";
+import { mkConstructiveDilemma, mkModusPonens } from "./propositional/tautology.ts";
+import { oakCors } from "cors/mod.ts";
 
-// トートロジー
-// 結合律
-print(associativeAnd);
-print(associativeOr);
-// 交換律
-print(commutativeAnd);
-print(commutativeOr);
-// 吸収律
-print(absorptiveAndOr);
-print(absorptiveOrAnd);
-// 冪等律
-print(idempotentAnd);
-print(idempotentOr);
-// 縮小律
-print(simplificationLeft);
-print(simplificationRight);
-// 拡大律
-print(additionLeft);
-print(additionRight);
-// 分配律
-print(distributiveAndOr);
-print(distributiveOrAnd);
-// 同一律
-print(identity);
-// 推移律
-print(transitive);
-// 移入律
-print(importation);
-// 移出律
-print(exportation);
-// De Morgan
-print(demorganAndOr);
-print(demorganOrAnd);
-// 対偶律
-print(contraposition);
-// 二重否定律
-print(doubleNegation);
-// 排中律
-print(excludeMiddle);
-// 矛盾律
-print(nonContradiction);
-// モーダスポネンス
-print(modusPonens);
-// 選言的三段論法
-print(disjunctiveSyllogism);
-// 構成的両刃論法
-print(constructiveDilemma);
+const app = new Application();
+const router = new Router();
 
-// 誤謬
-// 後件肯定
-print(affirmingConsequent);
-// 後件肯定
-print(denyingAntecedent);
-// 選言肯定
-print(affirmingDisjunct);
-// 誤謬の誤謬
-print(fallancyFallancy);
+router.get("/solve", (context) => {
+  Logger.debug(`access`);
+  const branch = toBranch(
+    createFirstBranch({
+      type: "NOT",
+      in: mkConstructiveDilemma(
+        mkModusPonens({ type: "PROP", id: "P" }, { type: "PROP", id: "Q" }),
+        mkModusPonens({ type: "PROP", id: "R" }, { type: "PROP", id: "S" }),
+        mkModusPonens({ type: "PROP", id: "T" }, { type: "PROP", id: "U" }),
+      ),
+    }),
+  );
+
+  context.response.body = branch;
+  return;
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.use(oakCors());
+
+Logger.debug(`Start listening`);
+await app.listen({ port: 8080 });
