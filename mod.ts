b7b99f7c81b5d6412780fcc60940de010b161454
SnO2WMaN <me@sno2wman.net>
2022-08-06 07:51:47 +0900

    api response

--- mod.ts
+++ mod.ts
@@ -2,8 +2,9 @@ import { oakCors } from "cors/mod.ts";
 import { Application, Router } from "oak/mod.ts";
 import { Logger } from "./logger.ts";
 import { evalBranch } from "./prop/branch.ts";
-import { findTopOrBot } from "./prop/find_top_or_bot.ts";
+import { findTB } from "./prop/find_tb.ts";
 import { parseFormula } from "./prop/parser.ts";
+import { show } from "./prop/show.ts";
 
 const app = new Application();
 const router = new Router();
@@ -15,18 +16,18 @@ router.get("/solve", ({ request, response }) => {
     Logger.debug(`/solve formula is missing`);
     return;
   }
-  Logger.debug(`/solve given formula is ${reqFormula}`);
   const formula = parseFormula(reqFormula);
   if (formula === null) {
     response.status = 400;
     Logger.debug(`/solve ${formula} cannot be parsed.`);
     return;
   }
-
+  Logger.debug(`/solve given formula is ${show(formula)}`);
   const branch = evalBranch({ stack: [["NOT", formula]], nodes: [], skip: [], props: {}, junction: null });
-  const valid = findTopOrBot(branch) === false;
+  const valid = findTB(branch, "TOP") === false;
+  Logger.debug(`/solve ${show(formula)} is ${valid ? "valid" : "invalid"}`);
 
-  response.body = { branch, valid };
+  response.body = { formula, branch, valid };
   return;
 });
 app.use(router.routes());
