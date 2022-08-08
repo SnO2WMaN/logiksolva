29ae65dad2586fc818755efbeb66717972a25715
SnO2WMaN <me@sno2wman.net>
2022-08-08 16:59:27 +0900

    add parse

--- prop/inference.ts
+++ prop/inference.ts
@@ -1,16 +1,29 @@
 import { Branch, evalBranch } from "./branch.ts";
 import { findTB } from "./find_tb.ts";
-import { parseFormula, show } from "./mod.ts";
-import { PropInference } from "./types.ts";
+import { show } from "./mod.ts";
+import { parseFormula } from "./parser.ts";
+import { PropFormula, PropInference } from "./types.ts";
 
 export const showInference = (i: PropInference): string => {
   return `${i.premise.map((f) => show(f)).join(",")}⊨${show(i.consequence)}`;
 };
 
 export const parseInference = (s: string): PropInference | null => {
-  const consequence = parseFormula(s);
-  if (!consequence) return null;
-  return { premise: [], consequence };
+  const e = /^([^⊨]*)⊨(.+)$/.exec(s);
+  if (!e) return null;
+
+  const [, pr, cq] = e;
+  const premise = pr !== "" ? pr.split(",").map((v) => parseFormula(v)) : [];
+
+  if (premise.some((v) => v === null)) return null;
+
+  const consequence = parseFormula(cq);
+  if (consequence === null) return null;
+
+  return {
+    premise: premise as PropFormula[],
+    consequence,
+  };
 };
 
 export const checkInference = (i: PropInference): { branch: Branch; valid: boolean } => {
