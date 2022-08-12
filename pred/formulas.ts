d571a7f47a1613f726f5e6468fa093885c1e0345
SnO2WMaN <me@sno2wman.net>
2022-08-12 21:53:15 +0900

    CNFに関して

--- pred/formulas.ts
+++ pred/formulas.ts
@@ -1,5 +1,6 @@
 import { lengthTerm } from "./terms.ts";
-import { PredFormula } from "./types.ts";
+import { Not, PredFormula } from "./types.ts";
+import { showFormula } from "./show.ts";
 
 export const lengthFormula = (f: PredFormula): number => {
   switch (f[0]) {
@@ -20,3 +21,81 @@ export const lengthFormula = (f: PredFormula): number => {
       return 1 + lengthTerm(f[1]) + lengthFormula(f[2]);
   }
 };
+
+export const toCNF = (f: PredFormula): PredFormula => {
+  switch (f[0]) {
+    case "PRED":
+      return f;
+    case "NOT":
+      return toCNFNot(f);
+    case "OR":
+      return f;
+    case "AND":
+      return ["OR", ["NOT", f[1]], ["NOT", f[2]]];
+    case "IMP":
+      return ["OR", ["NOT", f[1]], f[2]];
+    case "EQ":
+      return ["OR", ["AND", f[1], f[2]], ["AND", ["NOT", f[1]], ["NOT", f[2]]]];
+  }
+};
+
+export const toCNFNot = (f: Not): PredFormula => {
+  switch (f[1][0]) {
+    case "PRED":
+      return f;
+    case "TOP":
+      return ["TOP"];
+    case "BOT":
+      return ["TOP"];
+    case "NOT":
+      return f[1][1];
+    case "AND":
+      return ["OR", ["NOT", f[1][1]], ["NOT", f[1][2]]];
+    case "OR":
+      return ["AND", toCNFNot(["NOT", f[1][1]]), toCNFNot(["NOT", f[1][2]])];
+    case "IMP":
+      return ["AND", f[1][1], ["NOT", f[1][2]]];
+    case "EQ":
+      return [
+        "AND",
+        ["OR", f[1][1], f[1][2]],
+        ["OR", ["NOT", f[1][1]], ["NOT", f[1][2]]],
+      ];
+    case "FORALL":
+      return ["EXISTS", f[1][1], ["NOT", f[1][2]]];
+    case "EXISTS":
+      return ["FORALL", f[1][1], ["NOT", f[1][2]]];
+  }
+};
+
+console.dir(
+  toCNFNot([
+    "NOT",
+    [
+      "AND",
+      ["PRED", "P", ["NAME", "a"]],
+      ["PRED", "Q", ["NAME", "a"]],
+    ],
+  ]),
+  { depth: Number.MAX_SAFE_INTEGER },
+);
+
+console.log(showFormula(
+  [
+    "IMP",
+    [
+      "AND",
+      ["PRED", "N", ["NAME", "0"]],
+      [
+        "FORALL",
+        ["VAR", "i"],
+        [
+          "IMP",
+          ["PRED", "N", ["VAR", "i"]],
+          ["PRED", "N", ["OP", "s", ["VAR", "i"]]],
+        ],
+      ],
+    ],
+    ["PRED", "N", ["OP", "s", ["OP", "s", ["NAME", "0"]]]],
+  ],
+));
